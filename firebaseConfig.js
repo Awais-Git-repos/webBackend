6cf21884fd96d8cc380d2e58db15b527f7b3eda1
Awais-Git-repos <awaiszubair512@gmail.com>
2024-12-18 10:24:26 +0500

    changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getFirestore } = require("firebase-admin/firestore");
 
-// Import your service account key
-const serviceAccount = require("./serviceAccountKey.json");
+// // Import your service account key
+// const serviceAccount = require("./serviceAccountKey.json");
 
-// Initialize the Firebase Admin SDK
-initializeApp({
-  credential: cert(serviceAccount),
-});
+// // Initialize the Firebase Admin SDK
+// initializeApp({
+//   credential: cert(serviceAccount),
+// });
 
-// Get Firestore instance
-const db = getFirestore();
+// // Get Firestore instance
+// const db = getFirestore();
 
-module.exports = db;
+// module.exports = db;
